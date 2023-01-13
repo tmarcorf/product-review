3af41048d1686ffc5e85e34d968d4f481176d4fd
marcotrf <tmarco783@gmail.com>
2023-01-13 20:14:33 -0300

    Definindo serviço de avaliação

---
+++ src/ProductReview.Servicos/Implementacoes/ServicoDeAvaliacao.cs
@@ -0,0 +1,19 @@
+﻿using ProductReview.Negocio.Entidades;
+using ProductReview.Repositorio.Interfaces;
+using ProductReview.Servico.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReview.Servico.Implementacoes
+{
+    public class ServicoDeAvaliacao : ServicoPadrao<Avaliacao>, IServicoDeAvaliacao
+    {
+        public ServicoDeAvaliacao(IRepositorioDeAvaliacao repositorio) 
+            : base(repositorio)
+        {
+        }
+    }
+}
---
+++ src/ProductReview.Servicos/Interfaces/IServicoDeAvaliacao.cs
@@ -0,0 +1,13 @@
+﻿using ProductReview.Negocio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReview.Servico.Interfaces
+{
+    public interface IServicoDeAvaliacao : IServicoPadrao<Avaliacao>
+    {
+    }
+}
